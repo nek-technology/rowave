12973a618d399c55d895bd8b17e5772826008c6c
michaelrbx <startreksrbx@gmail.com>
2023-12-13 19:57:57 +0000

    Rerouting Moonwave Components

--- src/Components/MoonwaveComponents/Class.lua
+++ src/Components/MoonwaveComponents/Class.lua
@@ -13,14 +13,13 @@ local COMPONENT_ONLY_PROPERTIES = {
 
 local StudioComponents = script.Parent.Parent.StudioComponents
 local StudioComponentsUtil = StudioComponents:FindFirstChild("Util")
--- local themeProvider = require(StudioComponentsUtil.themeProvider)
+local themeProvider = require(StudioComponentsUtil.themeProvider)
 
 local stripProps = require(StudioComponentsUtil.stripProps)
 
 local Background = require(StudioComponents.Background)
 local Label = require(StudioComponents.Label)
 local Title = require(StudioComponents.Title)
-local ScrollFrame = require(StudioComponents.ScrollFrame)
 
 export type Variables = {
 	ClassName: string,
@@ -30,38 +29,32 @@ export type Variables = {
 return function(props, vars: Variables)
 	local Class = Background({
 		[Children] = {
-			ScrollFrame({
+			New("Frame")({
 				ZIndex = 1,
 				Size = UDim2.fromScale(1, 1),
-
-				CanvasScaleConstraint = Enum.ScrollingDirection.X,
-
-				UILayout = New("UIListLayout")({
-					SortOrder = Enum.SortOrder.LayoutOrder,
-					Padding = UDim.new(0, 7),
-				}),
-
-				UIPadding = New("UIPadding")({
-					PaddingLeft = UDim.new(0, 5),
-					PaddingRight = UDim.new(0, 5),
-					PaddingBottom = UDim.new(0, 10),
-					PaddingTop = UDim.new(0, 10),
-				}),
-
+				BackgroundColor3 = themeProvider:GetColor(
+					props.StudioStyleGuideColor or Enum.StudioStyleGuideColor.MainBackground, 
+					props.StudioStyleGuideModifier
+				),
 				[Children] = {
+					New("UIListLayout")({
+						SortOrder = Enum.SortOrder.LayoutOrder,
+						Padding = UDim.new(0, 7),
+					}),
+
 					Title({
 						Text = vars.ClassName,
-						TextSize = 35,
+						TextSize = 43,
 					}),
 					Label({
 						Text = vars.Description,
 					}),
-					props[Children],
+					props.Children,
 				},
 			}),
 		},
 	})
 
-	local hydrateProps = stripProps(props, COMPONENT_ONLY_PROPERTIES)
+	local hydrateProps = stripProps({}, COMPONENT_ONLY_PROPERTIES)
 	return Fusion.Hydrate(Class)(hydrateProps)
 end
